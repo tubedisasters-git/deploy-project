bd698fc4c1b11052002328a74012d673b2b820f5
tubedisasters-git <deegenerator@gmail.com>
2023-02-06 02:05:05 +1100

    changed fallback to blocking

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -35,7 +35,7 @@ export async function getStaticPaths() {
   client.close();
 
   return {
-    fallback: false,
+    fallback: blocking,
     paths: dataArray.map((meetup) => ({
       params: { meetupId: meetup._id.toString() },
     })),
